43215dbce263b4fd4f206618a2fb1ef9c48033d0
allenlee <trainlee1024@qq.com>
2020-09-15 21:50:43 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年09月09日 星期三 12时18分08秒
+ ************************************************************************/
+
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+#include <map>
+using namespace std;
+
+int &f(int &n, int m) {
+    n += m;
+    return n;
+}
+
+int main() {
+    
+    return 0;
+}
